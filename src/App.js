cae49923b09beefddc05a16bde5c195bf9aeee06
trbogi <trencsenyi.bogi@gmail.com>
2022-09-20 13:57:38 +0200

    Create form for adding new dream

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [newDream, setNewDream] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center w-full h-screen text-slate-50 bg-slate-900">
+        <header className="uppercase">
+            Dream Diary
+        </header>
+        {!newDream &&
+            <button className="bg-indigo-900 px-2 py-2 mx-2 my-2 rounded-md" onClick={() => setNewDream(true)}>Add new
+                dream</button>
+        }
+        {newDream &&
+        <div className="h-screen flex flex-col items-center bg-slate-800 px-6 md:my-6 mx-4 rounded-md w-full sm:max-w-3xl overflow-hidden">
+            <textarea className="w-full h-full bg-slate-700 my-4 px-2 py-2 rounded-md" placeholder="What were you dreaming about?"></textarea>
+            <button className="bg-indigo-900 px-2 py-2 mx-2 my-2 rounded-md"onClick={() => setNewDream(false)}>Save</button>
+        </div>}
     </div>
   );
 }
