57cd42f5dd4965a9ff2c7c98966121442293cd35
trbogi <trencsenyi.bogi@gmail.com>
2022-09-22 23:29:12 +0200

    Fix syntax error

--- src/components/Diary.js
+++ src/components/Diary.js
@@ -6,13 +6,13 @@ function Diary() {
         <div className="max-h-[85%] overflow-auto">
             <ol className="relative border-l border-gray-200 mx-8 dark:border-gray-700">
                 {DREAMS.map((dream) => (
-                <li className="ml-6 my-8">
+                <li key={dream.id} className="ml-6 my-8">
                     <span
                         className="flex absolute -left-3 justify-center items-center w-6 h-6 rounded-full ring-8 ring-gray-900 bg-blue-900">
                         <svg aria-hidden="true" className="w-3 h-3 text-blue-400" fill="currentColor"
                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                clip-rule="evenodd"></path>
+                            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                clipRule="evenodd"></path>
                         </svg>
                     </span>
                     <h3 className="mb-1 text-lg font-semibold text-white">{dream.title}</h3>
