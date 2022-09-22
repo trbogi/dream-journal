e7d8f15033b33395f1c81565c060f5b4892ea89e
trbogi <trencsenyi.bogi@gmail.com>
2022-09-22 15:39:27 +0200

    Show dreams

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Diary from "../src/components/Diary"
 
 function App() {
     const [newDream, setNewDream] = useState(false);
@@ -7,27 +8,31 @@ function App() {
     const [dreamText, setDreamText] = useState("");
 
   return (
-    <div className="flex flex-col items-center w-full h-screen text-slate-50 bg-slate-900">
+    <div className="flex flex-col items-center w-full h-screen text-slate-50 bg-slate-900 overflow-hidden">
         <header>
             Dream Diary
         </header>
         {!newDream &&
-            <button className="bg-indigo-900 px-2 py-2 mx-2 my-2 rounded-md" onClick={() => setNewDream(true)}>Add new
+            <div className="max-w-5xl max-h-full">
+                <button className="bg-blue-600 font-bold px-2 py-2 mx-2 my-2 rounded-md" onClick={() => setNewDream(true)}>Add new
                 dream</button>
+                <Diary></Diary>
+            </div>
+
         }
         {newDream &&
-        <div className="h-screen flex flex-col items-center bg-slate-800 px-6 md:my-6 mx-4 rounded-md w-full sm:max-w-3xl overflow-hidden">
+        <div className="h-full flex flex-col items-center bg-slate-800 px-6 md:my-6 mx-4 rounded-md w-full sm:max-w-3xl overflow-hidden">
             <div className="flex flex-row w-full">
                 <input type="date" lang="en" value={date} onChange={(e) => setDate(e.target.value)}
-                       className="bg-slate-700 border my-4 mr-4 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 0"
+                       className="bg-slate-700 my-4 mr-4 px-2 py-2 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 0"
                        style={{"colorScheme": "dark"}} placeholder="Select date"/>
                 <input type="text" onChange={(e) => setTitle(e.target.value)}
-                       className="bg-slate-700 border my-4 ml-4 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 0" placeholder="Title"/>
+                       className="bg-slate-700 my-4 ml-4 px-2 py-2 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 0" placeholder="Title"/>
             </div>
 
             <textarea className="w-full h-full bg-slate-700 my-4 px-2 py-2 rounded-md" placeholder="What were you dreaming about?"
                       onChange={(e) => setDreamText(e.target.value)}></textarea>
-            <button className="bg-indigo-900 px-2 py-2 mx-2 my-2 rounded-md"onClick={() => setNewDream(false)}>Save</button>
+            <button className="bg-blue-600 font-bold px-2 py-2 mx-2 mb-4 rounded-md"onClick={() => setNewDream(false)}>Save</button>
         </div>}
     </div>
   );
