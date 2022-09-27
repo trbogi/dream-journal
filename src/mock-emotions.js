5c46191d0368da45b416b92095de9760ea5f2b77
trbogi <trencsenyi.bogi@gmail.com>
2022-09-27 14:53:18 +0200

    Create mock-emotions.js

---
+++ src/mock-emotions.js
@@ -0,0 +1,31 @@
+export const EMOTIONS = [
+    "admiration",
+    "adoration",
+    "aesthetic appreciation",
+    "amusement",
+    "anger",
+    "anxiety",
+    "awe",
+    "awkwardness",
+    "boredom",
+    "calmness",
+    "confusion",
+    "craving",
+    "disgust",
+    "empathic pain",
+    "entrancement",
+    "excitement",
+    "fear",
+    "horror",
+    "interest",
+    "joy",
+    "nostalgia",
+    "relief",
+    "romance",
+    "sadness",
+    "satisfaction",
+    "sexual desire",
+    "surprise"
+]
+
+export const suggestions = EMOTIONS.map((name, index) => ({ value: index, label: name }))
