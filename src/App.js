c7a693d05347c3c4906391ef106ee3e59ec6e781
trbogi <trencsenyi.bogi@gmail.com>
2022-10-14 21:37:36 +0200

    Iconify add and filter buttons on small screens

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import {useState} from "react";
 import Diary from "../src/components/Diary"
 import NewDream from "./components/NewDream"
 import {IoSearch} from "react-icons/io5"
+import {FiFilter} from 'react-icons/fi'
+import {BsPencilSquare} from 'react-icons/bs'
 import FilterByTime from "./components/FilterByTime";
 import FilterByEmotions from "./components/FilterByEmotions";
-
 
 function App() {
     const [newDream, setNewDream] = useState(false);
@@ -12,6 +13,7 @@ function App() {
     const [fromDate, setFromDate] = useState("");
     const [toDate, setToDate] = useState("");
     const [tags, setTags] = useState([]);
+    const [showFilters, setShowFilters] = useState(false)
 
     const search = (e) => {
         setSearchPhrase(e.target.value)
@@ -22,16 +24,22 @@ function App() {
         {!newDream &&
             <div className="max-w-5xl max-h-full min-w-full lg:min-w-[1024px] lg:flex lg:mt-10">
                 <div className="flex justify-between mb-4 px-4 flex-col lg:justify-start">
-                    <div className="flex justify-between w-full lg:flex-col">
-                        <button className="min-w-max text-xs sm:text-sm bg-blue-600 font-bold px-2 py-2 my-2 rounded-full lg:mb-16" onClick={() => setNewDream(true)}>Add new
-                            dream</button>
-                        <div className="flex items-center bg-slate-300 text-slate-900 rounded-full px-4 py-2 my-2">
-                            <IoSearch/>
-                            <input className="bg-slate-300 px-2 focus:outline-none" type="search" onChange={search}/>
+                    <div className="flex justify-between  w-full lg:flex-col">
+                        <button className="text-sm bg-blue-600 font-bold px-2 py-2 my-2 rounded-full lg:mb-16" onClick={() => setNewDream(true)}>
+                            <span className="hidden sm:block">Add new dream</span> <BsPencilSquare className="w-5 h-5 my-1 mx-1 sm:hidden"/></button>
+                        <div className="flex items-center gap-1">
+                            <div className="flex items-center bg-slate-300 text-slate-900 rounded-full px-4 py-1 my-2">
+                                <IoSearch/>
+                                <input className="bg-slate-300 px-2 focus:outline-none" type="search" onChange={search}/>
+                            </div>
+                            <button className={`w-8 h-8 flex justify-center items-center rounded-full bg-slate-300 text-slate-900 my-2 hover:text-blue-700 ${showFilters? "bg-slate-400": ""} lg:hidden`}
+                                    onClick={() => setShowFilters(!showFilters)}><FiFilter/></button>
                         </div>
-                    </div>
-                    <FilterByTime from={fromDate} to={toDate} setFrom={setFromDate} setTo={setToDate}/>
-                    <FilterByEmotions tags={tags} setTags={setTags}/>
+                     </div>
+                        <div className={`${showFilters? "": "hidden"} lg:block`}>
+                            <FilterByTime from={fromDate} to={toDate} setFrom={setFromDate} setTo={setToDate}/>
+                            <FilterByEmotions tags={tags} setTags={setTags}/>
+                        </div>
                 </div>
                 <Diary searchPhrase={searchPhrase} fromDate={fromDate} toDate={toDate} tags={tags}/>
             </div>
