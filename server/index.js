cb8c0118491e9ec93597d57fd9afabe6627e2028
trbogi <trencsenyi.bogi@gmail.com>
2023-01-04 12:47:49 +0100

    Add /getEmotions endpoint

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const DreamModel = require('./models/Dream')
+const { EmotionModel }= require('./models/Emotion')
 
 mongoose.connect('mongodb+srv://trbogi:Pass8trbogi@cluster0.z5f1okn.mongodb.net/dream-journal?retryWrites=true&w=majority')
 
 app.get('/getDreams', (req, res) => {
     DreamModel.find({}, (err, result) => {
+        if (err){
+            res.json(err)
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+app.get('/getEmotions', (req, res) => {
+    EmotionModel.find({}, (err, result) => {
         if (err){
             res.json(err)
         }else{
