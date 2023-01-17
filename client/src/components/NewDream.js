fee7664b1b88dcab72034e5f25a4ba52282df940
trbogi <trencsenyi.bogi@gmail.com>
2023-01-17 12:43:27 +0100

    Buttons of NewDream component works with routes

--- client/src/components/NewDream.js
+++ client/src/components/NewDream.js
@@ -1,7 +1,9 @@
-import {useState} from "react";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 import Selector from './Selector'
 
 function NewDream() {
+    const navigate = useNavigate()
     const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
     const [title, setTitle] = useState("");
     const [emotions, setEmotions] = useState([]);
@@ -26,6 +28,7 @@ function NewDream() {
     const add = (e) => {
         e.preventDefault()
         addNewDream()
+        navigate('/')
     }
 
     return (
@@ -42,7 +45,8 @@ function NewDream() {
             <textarea className="w-full h-full bg-slate-700 border border-slate-700 my-4 px-2 py-2 rounded-2xl focus:outline-none focus:border-blue-500 text-sm" placeholder="What were you dreaming about?"
                       required={true} onChange={(e) => setText(e.target.value)}/>
             <div className="w-full flex justify-between">
-                <button className="text-blue-400 text-sm border border-blue-400 px-4 py-2 mb-4 rounded-full" >Cancel</button>
+                <button className="text-blue-400 text-sm border border-blue-400 px-4 py-2 mb-4 rounded-full"
+                type="button" onClick={() => navigate(-1)}>Cancel</button>
                 <button type="submit" className="bg-blue-600 font-bold text-sm px-4 py-2 mb-4 rounded-full">Save</button>
             </div>
     </form>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
-import {useState} from "react"
-import {IoSearch} from "react-icons/io5"
-import {FiFilter} from 'react-icons/fi'
-import {BsPencilSquare} from 'react-icons/bs'
+import { useState } from "react"
+import { IoSearch } from "react-icons/io5"
+import { FiFilter } from 'react-icons/fi'
+import { BsPencilSquare } from 'react-icons/bs'
 import FilterByTime from "../components/FilterByTime"
 import FilterByEmotions from "../components/FilterByEmotions"
 import Diary from "../components/Diary"
+import { Link } from "react-router-dom"
 
 function Home() {
     const [searchPhrase, setSearchPhrase] = useState("");
@@ -23,8 +24,11 @@ function Home() {
         {/*Menu*/}
         <div className="flex justify-between mb-4 px-4 flex-col lg:justify-start lg:max-w-[260px]">
             <div className="flex justify-between w-full lg:flex-col">
-                <button className="text-sm bg-blue-600 font-bold px-2 py-2 my-2 rounded-full lg:mb-16" >
-                    <span className="hidden sm:block">Add new dream</span> <BsPencilSquare className="w-5 h-5 my-1 mx-1 sm:hidden"/></button>
+                <Link to='/new'>
+                    <div className="flex justify-center text-sm bg-blue-600 font-bold px-2 py-2 my-2 rounded-full lg:mb-16" >
+                        <span className="hidden sm:block">Add new dream</span> <BsPencilSquare className="w-5 h-5 my-1 mx-1 sm:hidden"/>
+                    </div>
+                </Link>
                 <div className="flex items-center gap-1">
                     <div className="flex items-center w-full  bg-slate-300 text-slate-900 rounded-full px-4 py-1 my-2">
                         <IoSearch/>
