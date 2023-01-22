a9182f30a3b56edcdb272209419e1b7cf9ae2670
trbogi <trencsenyi.bogi@gmail.com>
2023-01-22 21:49:41 +0100

    Add modal for delete confirmation

--- client/src/components/DreamItem.js
+++ client/src/components/DreamItem.js
@@ -1,10 +1,13 @@
 import { useDreamsContext } from "../hooks/useDreamsContext"
 import Highlighter from "react-highlight-words"
 import { FiEdit, FiTrash2 } from "react-icons/fi"
+import { AiOutlineExclamationCircle } from "react-icons/ai"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function DreamItem({dream, searchPhrase}) {
     const { dispatch } = useDreamsContext()
+    const [showModal, setShowModal] = useState(false)
 
     const deleteDream = async () => {
         const response = await fetch(`http://localhost:3001/api/dreams/${dream._id}`, {
@@ -18,6 +21,7 @@ function DreamItem({dream, searchPhrase}) {
     }
 
     return (
+        <>
         <li className="ml-6 my-8">
             <div className="flex justify-between items-center">
                                                     <span
@@ -40,7 +44,7 @@ function DreamItem({dream, searchPhrase}) {
                         className="block mb-2 text-ml font-bold leading-none text-blue-400">{new Date(dream.date).toDateString()}
                     </time>
                     
-                        <button className="px-1 mb-2" onClick={deleteDream}><FiTrash2/></button>
+                        <button className="px-1 mb-2" onClick={() => setShowModal(true)}><FiTrash2/></button>
                         <Link to={`/edit/${dream._id}`}><button className="pl-1 mb-2"><FiEdit/></button></Link>
                     
                 </div>
@@ -60,7 +64,37 @@ function DreamItem({dream, searchPhrase}) {
                 />
             </p>
         </li>
-    );
+        {showModal && 
+        <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+
+            <div className="relative transform overflow-hidden rounded-lg bg-blue-50 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                    <AiOutlineExclamationCircle className="h-12 w-12 text-blue-600"/>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">Are you sure you want to delete this dream?</h3>
+
+                  </div>
+                </div>
+              </div>
+              <div className="px-4 py-3 sm:flex sm:justify-between sm:flex-row-reverse sm:px-6">
+                <button type="button" className="inline-flex w-full justify-center rounded-full border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
+                <button type="button" className="mt-3 inline-flex w-full justify-center rounded-full border border-blue-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50  sm:mt-0 sm:w-auto sm:text-sm"
+                onClick={() => setShowModal(false)}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        }
+    </>
+    )
 }
 
 export default DreamItem;
