fc86e9c56d6ea7f7a6af08a4325680298321374c
trbogi <trencsenyi.bogi@gmail.com>
2023-01-23 23:03:28 +0100

    Change route paths

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,9 @@ function App() {
         <div className="flex flex-col items-center w-full h-screen font-quicksand text-slate-50 bg-slate-900 overflow-auto overflow-auto">
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/new' element={<DreamForm/>}/>
-                    <Route path='/edit/:id' element={<DreamForm/>}/>
+                    <Route path='/dream-journal' element={<Home/>}/>
+                    <Route path='/dream-journal/new' element={<DreamForm/>}/>
+                    <Route path='/dream-journal/edit/:id' element={<DreamForm/>}/>
                 </Routes>
              </BrowserRouter>
         </div>
