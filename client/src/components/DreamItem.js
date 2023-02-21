80a49f5bf6a1dc93ebbc271b68db1bab44b43520
trbogi <trencsenyi.bogi@gmail.com>
2023-02-21 13:40:32 +0100

    Fix mobile view issues

--- client/src/components/DreamItem.js
+++ client/src/components/DreamItem.js
@@ -23,15 +23,14 @@ function DreamItem({dream, searchPhrase}) {
     return (
         <>
         <li className="ml-6 my-8">
-            <div className="flex justify-between items-center">
-                                                    <span
-                                                        className="flex absolute -left-3 justify-center items-center w-6 h-6 rounded-full ring-8 ring-gray-900 bg-blue-900">
-                                <svg aria-hidden="true" className="w-3 h-3 text-blue-400" fill="currentColor"
-                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                          clipRule="evenodd"></path>
-                                </svg>
-                            </span>
+            <div className="flex flex-col w-full sm:flex-row sm:justify-between sm:items-center">
+                <span className="flex absolute -left-3 justify-center items-center w-6 h-6 rounded-full ring-8 ring-gray-900 bg-blue-900">
+                    <svg aria-hidden="true" className="w-3 h-3 text-blue-400" fill="currentColor"
+                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                              clipRule="evenodd"></path>
+                    </svg>
+                </span>
                 <h3 className="mb-1 text-lg font-semibold text-white">
                     <Highlighter
                         searchWords={[searchPhrase]}
@@ -43,10 +42,10 @@ function DreamItem({dream, searchPhrase}) {
                     <time
                         className="block mb-2 text-ml font-bold leading-none text-blue-400">{new Date(dream.date).toDateString()}
                     </time>
-                    
+                    <div>
                         <button className="px-1 mb-2" onClick={() => setShowModal(true)}><FiTrash2/></button>
                         <Link to={`/edit/${dream._id}`}><button className="pl-1 mb-2"><FiEdit/></button></Link>
-                    
+                    </div>
                 </div>
             </div>
             {dream.emotions &&
